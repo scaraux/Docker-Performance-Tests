adc55409d2ab91993308d27eab28a3357d8ae5d7
Jeremy Octeau <octeau.jeremy@gmail.com>
2019-05-09 15:46:20 -0700

    cpu %

--- CPU/Hash/hash_crypto.py
+++ CPU/Hash/hash_crypto.py
@@ -1,8 +1,21 @@
 from timeit import default_timer as timer
 from datetime import timedelta
 import hashlib
+import psutil
+from threading import Thread
 
-DATA = open('test-data.html').read().encode()
+DATA = open('data.txt').read().encode()
+
+def cpu_usage(stop):
+    i = 0
+    total = 0
+    while True:
+        if stop():
+            print('Average CPU use: {0:.2f}%'.format(total / i))
+            break
+        total += psutil.cpu_percent(interval=1)
+        i += 1
+
 
 def proof_of_work(nonce):
     sha = hashlib.sha256(DATA + str(nonce).encode())
@@ -16,12 +29,19 @@ if __name__ == "__main__":
     start = timer()
 
     target = 0x0000FFFFFFFFFFFFFFFFFFFFFFF000000000000000000000000000000000000
+    # target = 0x0000112FFFFFFFFFFFFFFFFFFFF000000000000000000000000000000000000
     nonce = 0
     final_hash = proof_of_work(nonce)
+
+    stop_threads = False
+    thread = Thread(target = cpu_usage, args =(lambda : stop_threads, ))
+    thread.start()
+
     while (int(final_hash, 16) > int(target)):
         nonce += 1
         final_hash = proof_of_work(nonce)
 
     end = timer()
-    print('Final Hash:', final_hash)
+    stop_threads = True
+    print('Final hash:', final_hash)
     print('Time elapsed:', timedelta(seconds=end-start))
